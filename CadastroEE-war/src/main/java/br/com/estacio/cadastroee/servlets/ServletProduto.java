60c1060e36e9ac50813cbd490538180a28991348
Larissa <larissagnandt@gmail.com>
2025-05-27 18:49:46 -0300

    feat: ajuste nos importes por conta da v ersão do java

--- CadastroEE-war/src/main/java/br/com/estacio/cadastroee/servlets/ServletProduto.java
+++ CadastroEE-war/src/main/java/br/com/estacio/cadastroee/servlets/ServletProduto.java
@@ -5,12 +5,12 @@ import br.com.estacio.cadastroee.model.Produto;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import javax.ejb.EJB;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ServletProduto", urlPatterns = { "/ServletProduto" })
 public class ServletProduto extends HttpServlet {
